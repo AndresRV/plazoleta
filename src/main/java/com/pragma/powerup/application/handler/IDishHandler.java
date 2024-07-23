d01e9599cc475966693ed3128c3d3463c35ceaee
Andres Ramirez Villareal <andres1743@hotmail.com>
2024-07-23 18:29:57 -0500

    Validate if user request is the owner

--- src/main/java/com/pragma/powerup/application/handler/IDishHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/IDishHandler.java
@@ -3,6 +3,6 @@ package com.pragma.powerup.application.handler;
 import com.pragma.powerup.application.dto.request.DishRequest;
 
 public interface IDishHandler {
-    void saveDish(DishRequest dishRequest);
-    void updateDish(DishRequest dishRequest);
+    void saveDish(DishRequest dishRequest, Long idUserRequest);
+    void updateDish(DishRequest dishRequest, Long idUserRequest);
 }
--- src/main/java/com/pragma/powerup/domain/usecase/DishUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/DishUseCase.java
@@ -1,17 +1,22 @@
 package com.pragma.powerup.domain.usecase;
 
 import com.pragma.powerup.domain.api.IDishServicePort;
+import com.pragma.powerup.domain.exception.InvalidPhoneNumberException;
+import com.pragma.powerup.domain.exception.InvalidUserRequestIsNotOwner;
 import com.pragma.powerup.domain.model.Dish;
 import com.pragma.powerup.domain.spi.IDishPersistencePort;
 import lombok.RequiredArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 public class DishUseCase implements IDishServicePort {
     private final IDishPersistencePort dishPersistencePort;
 
     @Override
-    public void saveDish(Dish dish) {
-        //TODO: valiadar solo propietario (Solo el propietario de un restaurante puede crear platos)
+    public void saveDish(Dish dish, boolean userRequestIsOwner) {
+        validateUserRequestIsOwner(userRequestIsOwner);
         dish.setActive(true);
         dishPersistencePort.saveDish(dish);
     }
@@ -22,7 +27,13 @@ public class DishUseCase implements IDishServicePort {
     }
 
     @Override
-    public void updateDish(Dish dish) {
+    public void updateDish(Dish dish, boolean userRequestIsOwner) {
+        validateUserRequestIsOwner(userRequestIsOwner);
         dishPersistencePort.updateDish(dish);
+    }
+
+    private static void validateUserRequestIsOwner(boolean userRequestIsOwner) {
+        if (!userRequestIsOwner)
+            throw new InvalidUserRequestIsNotOwner("Request user  is not the owner of the restaurant");
     }
 }
